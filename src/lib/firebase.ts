2b02805ae3f47c15e147087b44b8a1972aec0a3f
reginaldusgabur <reginaldusgabur@gmail.com>
2026-04-07 13:49:31 +0000

    Fix: Simplify Firebase Admin initialization

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,27 +1,15 @@
 
 import admin from "firebase-admin";
 
-// Construct the service account object from environment variables
-const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  // The private key needs to have its newlines restored
-  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-};
+// The Firebase Admin SDK will automatically find and use the credentials
+// from the GOOGLE_APPLICATION_CREDENTIALS environment variable when initialized.
+// We just need to make sure the app is initialized.
 
 if (!admin.apps.length) {
+  // When GOOGLE_APPLICATION_CREDENTIALS is set, initializeApp() will use it.
+  // We can also provide other config like the databaseURL here.
   admin.initializeApp({
-    // Use the constructed service account object for credentials
-    credential: admin.credential.cert(serviceAccount as any),
-    databaseURL: "https://studio-6483313657-7894b-default-rtdb.firebaseio.com"
+      databaseURL: "https://studio-6483313657-7894b-default-rtdb.firebaseio.com"
   });
 }
 
